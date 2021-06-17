ac0e828f6e14057b35360c51bc507fab6f3e382a
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-06-18 00:23:57 +0900

    Base Project need to confirm

---
+++ MogaStyle/app/src/main/java/com/example/mogastyle/Activities/Consult/ConsultMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mogastyle.Activities.Consult;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConsultMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consult_main);
+    }
+}
---
+++ MogaStyle/app/src/main/java/com/example/mogastyle/Activities/Hair/HairMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mogastyle.Activities.Hair;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HairMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hair_main);
+    }
+}
