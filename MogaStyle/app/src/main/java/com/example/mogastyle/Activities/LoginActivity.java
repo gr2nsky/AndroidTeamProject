eb2fd8e422ab5f1a6e09188685f80a1536067ba1
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-06-20 18:34:02 +0900

    Base Project GoogleLogin

--- MogaStyle/app/src/main/java/com/example/mogastyle/Activities/LoginActivity.java
+++ MogaStyle/app/src/main/java/com/example/mogastyle/Activities/LoginActivity.java
@@ -5,6 +5,8 @@
  */
 package com.example.mogastyle.Activities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -21,6 +23,19 @@ import android.widget.Toast;
 
 import com.example.mogastyle.Common.LoginedUserInfo;
 import com.example.mogastyle.R;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.kakao.auth.ISessionCallback;
 import com.kakao.auth.Session;
 import com.kakao.network.ErrorResult;
@@ -29,9 +44,11 @@ import com.kakao.usermgmt.callback.MeV2ResponseCallback;
 import com.kakao.usermgmt.response.MeV2Response;
 import com.kakao.util.exception.KakaoException;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.security.MessageDigest;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     Button btn_login_goMain;
     TextView tv_login_another_way_login;
@@ -39,6 +56,13 @@ public class LoginActivity extends AppCompatActivity {
     //KAKAO LOGIN
     private ISessionCallback kakaoSessionCallback;
     //-- KAKAO END
+
+    //GOOGLE LOGIN
+    private SignInButton btn_login_google_login;
+    private FirebaseAuth auth;
+    private GoogleApiClient googleApiClient;
+    private static final int REQ_SIGN_GOOGLE = 100;
+    //-- GOOGLE END
 
     Intent intent;
 
@@ -98,6 +122,26 @@ public class LoginActivity extends AppCompatActivity {
         Session.getCurrentSession().checkAndImplicitOpen();
         //-- KAKAO END
 
+        // GOOGLE LOGIN
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API , googleSignInOptions)
+                .build();
+
+        auth = FirebaseAuth.getInstance(); // firebase 인증 객체 초기화
+
+        btn_login_google_login = findViewById(R.id.btn_login_google_login);
+        btn_login_google_login.setOnClickListener(onClickListener);
+        //--GOOGLE LOGIN END
+
+
+
+
         // 반영후에는 삭제될 메소드
         getAppKeyHash();
     }
@@ -116,6 +160,10 @@ public class LoginActivity extends AppCompatActivity {
                     intent = new Intent(LoginActivity.this , LoginBasicActivity.class);
                     startActivity(intent);
                     break;
+
+                case R.id.btn_login_google_login:
+                    intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                    startActivityForResult(intent , REQ_SIGN_GOOGLE);
             }
         }
     };
@@ -145,4 +193,53 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    // GOOGLE LOGIN
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        //Google LOGIN AUTH REQUEST , 결과값을 되돌려 받는곳
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQ_SIGN_GOOGLE){
+            GoogleSignInResult googleSignInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+            // 인증 결과 성공 상황
+            if(googleSignInResult.isSuccess()){
+                GoogleSignInAccount googleSignInAccount = googleSignInResult.getSignInAccount(); // googleSignInAccount 에 구글 로그인 정보 저장 (닉네임, 프로필 사진 URL, 이메일 주소...)
+                resultGoogleLogin(googleSignInAccount);
+            }
+        }
+
+    }
+
+    private void resultGoogleLogin(GoogleSignInAccount googleSignInAccount) {
+        AuthCredential authCredential = GoogleAuthProvider.getCredential(googleSignInAccount.getIdToken() , null);
+        auth.signInWithCredential(authCredential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                        // 로그인에 실제 성공 했는지 상황
+                        if(task.isSuccessful()) {
+                            // 로그인 성공 상황
+                            Toast.makeText(LoginActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
+                            intent = new Intent(getApplicationContext() , MainActivity.class);
+                            intent.putExtra("nickName" , googleSignInAccount.getDisplayName());
+                            // 이름 저장
+                            LoginedUserInfo.user.setName(googleSignInAccount.getDisplayName());
+                            // --
+                            intent.putExtra("photo" , googleSignInAccount.getPhotoUrl());
+
+                            startActivity(intent);
+                        }else{
+                            //로그인 실패 상황
+                            Toast.makeText(LoginActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull @NotNull ConnectionResult connectionResult) {
+
+    }
+    // -- GOOGLE LOGIN END
 }
