67fb3f75be9386021e69b20fcbbbad475f76a4fd
gr2nsky <dbswovlf2009@naver.com>
2021-06-22 04:55:19 +0900

    reservation 가상의 데이터들로 출력 완료
    1. 매장 정보 및 메뉴 정보와 레이아웃 아이템 바인딩 완료
    2. 디자이너 사진 및 경력 과 레이아웃 아이템 바인딩 완료
    3. 일자별 예약된 시간은 제외하고 출력
    4. 기본으로 '오늘'이 선택되어 있음
    5. 샵, 디자이너 휴무일 반영
    6. 예약가능 날자가 꽉 찼을경우 예외처리 완료
    ---------------------------------
    - 이것만 하면 완성!
    디자이너의 no로 오늘로부터 30일 내의 예약을 db로부터 불러오기

---
+++ MogaStyle/app/src/main/java/com/example/mogastyle/Bean/TempDesignerBean.java
@@ -0,0 +1,85 @@
+/*
+
+제작 : 윤재필
+20210621 테스트용으로 임시로 제작함.
+
+ */
+package com.example.mogastyle.Bean;
+
+import java.util.ArrayList;
+
+public class TempDesignerBean {
+    int no;
+    String name;
+    String certificationDate;
+    ArrayList<ResDateData> resDates;
+    String img;
+    ArrayList<Integer> holidays;
+
+    public TempDesignerBean() {
+    }
+
+    public TempDesignerBean(String name, String certificationDate, ArrayList<ResDateData> resDates, String img, String holiday) {
+        this.name = name;
+        this.certificationDate = certificationDate;
+        this.resDates = resDates;
+        this.img = img;
+        this.holidays = new ArrayList<>();
+        if(holiday.length() > 2) {
+            String[] holidaySplited = holiday.split(",");
+            for(String s : holidaySplited){
+                holidays.add(Integer.parseInt(s));
+            }
+        } else {
+            holidays.add(Integer.parseInt(holiday));
+        }
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCertificationDate() {
+        return certificationDate;
+    }
+
+    public void setCertificationDate(String certificationDate) {
+        this.certificationDate = certificationDate;
+    }
+
+    public ArrayList<ResDateData> getResDates() {
+        return resDates;
+    }
+
+    public void setResDates(ArrayList<ResDateData> resDates) {
+        this.resDates = resDates;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public ArrayList<Integer> getHolidays() {
+        return holidays;
+    }
+
+    public void setHolidays(ArrayList<Integer> holidays) {
+        this.holidays = holidays;
+    }
+}
