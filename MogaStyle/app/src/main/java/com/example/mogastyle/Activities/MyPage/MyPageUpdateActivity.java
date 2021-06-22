bf0d240b7a722641f0fe2531d80f138e7f7af29a
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-06-22 15:36:53 +0900

    userInfo

---
+++ MogaStyle/app/src/main/java/com/example/mogastyle/Activities/MyPage/MyPageUpdateActivity.java
@@ -0,0 +1,16 @@
+package com.example.mogastyle.Activities.MyPage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mogastyle.R;
+
+public class MyPageUpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_page_update);
+    }
+}
