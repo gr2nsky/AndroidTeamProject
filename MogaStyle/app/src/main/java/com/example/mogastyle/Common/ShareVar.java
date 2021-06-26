5df1b9ce10fa1d6267de57335db62e02ea94e239
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-06-26 20:27:06 +0900

    rebase

--- MogaStyle/app/src/main/java/com/example/mogastyle/Common/ShareVar.java
+++ MogaStyle/app/src/main/java/com/example/mogastyle/Common/ShareVar.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 public class ShareVar {
 
 
-    public static String hostIP = "";
+    public static String hostIP = "192.168.35.80";
     public final static String hostRootAddr = "http://" + hostIP + ":8080/MogaStyle/";
     public final static String userImgPath = hostRootAddr + "img/user/";
     public final static String shopImgPath = hostRootAddr + "img/shop/";
