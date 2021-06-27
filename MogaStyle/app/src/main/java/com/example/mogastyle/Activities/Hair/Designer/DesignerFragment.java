c081ab266c414235a23ea03b2f7ef3fcaee3f683
Jiyeon Choi <cjy3094@naver.com>
2021-06-27 14:37:14 +0900

    Fragment
    Fragment

--- MogaStyle/app/src/main/java/com/example/mogastyle/Activities/Hair/Designer/DesignerFragment.java
+++ MogaStyle/app/src/main/java/com/example/mogastyle/Activities/Hair/Designer/DesignerFragment.java
@@ -1,39 +1,47 @@
 package com.example.mogastyle.Activities.Hair.Designer;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.example.mogastyle.Bean.Designer;
+import com.example.mogastyle.Bean.Styling;
 import com.example.mogastyle.R;
 
 public class DesignerFragment extends Fragment {
+    Context con;
+    Designer DesignerBean = null;
+    ListView listView;
 
-    public DesignerFragment() {
-        // Required empty public constructor
+
+    public DesignerFragment(Designer DesignerBean, Context con) {
+        this.DesignerBean = DesignerBean;
+        this.con = con;
     }
 
 
-    public static DesignerFragment newInstance(String param1, String param2) {
-        DesignerFragment fragment = new DesignerFragment();
-        Bundle args = new Bundle();
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_designer_list,container,false);
 
-        return fragment;
+        listView = view.findViewById(R.id.lv_designer_list);
+
+
+
+
+
+        return view;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_designer_list, container, false);
-    }
-}
+} // -----------------------------------
--- MogaStyle/app/src/main/java/com/example/mogastyle/Adapters/Hair/Shop/ShopPagerAdapter.java
+++ MogaStyle/app/src/main/java/com/example/mogastyle/Adapters/Hair/Shop/ShopPagerAdapter.java
@@ -11,7 +11,9 @@ import com.example.mogastyle.Activities.Review.ReviewFragment;
 import com.example.mogastyle.Activities.Hair.Designer.DesignerFragment;
 import com.example.mogastyle.Activities.Hair.Menu.MenuFragment;
 import com.example.mogastyle.Activities.Hair.Shop.ShopHomeFragment;
+import com.example.mogastyle.Bean.Designer;
 import com.example.mogastyle.Bean.Shop;
+import com.example.mogastyle.Bean.Styling;
 
 import java.util.ArrayList;
 
@@ -23,20 +25,24 @@ public class ShopPagerAdapter extends FragmentPagerAdapter {
     //       Shop을 Shop home activity로부터 받아옴        //
     /////////////////////////////////////////////////////////
     Shop shopBean = null;
+    Styling StylingBean = null;
+    Designer DesignerBean = null;
     Context con;
 
-    public ShopPagerAdapter(FragmentManager fm, Shop shopBean, Context con) {
+    public ShopPagerAdapter(FragmentManager fm, Shop shopBean, Styling StylingBean, Designer DesignerBean, Context con) {
         super(fm);
         this.shopBean = shopBean;
+        this.StylingBean = StylingBean;
+        this.DesignerBean = DesignerBean;
         this.con = con;
 
         arrayList.add(new ShopHomeFragment(shopBean, con));
-        arrayList.add(new MenuFragment());
-        arrayList.add(new DesignerFragment());
+        arrayList.add(new MenuFragment(StylingBean, con));
+        arrayList.add(new DesignerFragment(DesignerBean, con));
         arrayList.add(new ReviewFragment(shopBean, con));
 
         name.add("홈");
-        name.add("메뉴");
+        name.add("스타일링");
         name.add("디자이너");
         name.add("리뷰");
     
